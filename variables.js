ccee32bb862b96b598bbf7c5d8739e3e97b8fb87
Kiptoo <denysmutay@gmail.com>
2021-02-08 15:18:26 +0300

    added practise file for variables in JS

---
+++ variables.js
@@ -0,0 +1,30 @@
+console.log('Welcome To Asgard')
+
+// Declare two variables: admin and name
+
+let admin;
+let naMe;
+
+// Assign the value "John" to name
+
+naMe = "John";
+
+// Copy the value from name to admin.
+
+admin = naMe;
+
+// Show the value of admin using alert (must output “John”).
+
+alert(admin);
+
+
+// Create a variable with the name of our planet. How would you name such a variable?
+
+let ourPlanetName;
+ourPlanetName = "Earth";
+
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+let currentUserName;
+currentUserName = "Kiptoo Dennis";
+
